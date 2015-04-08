75f7de99bb212ff483738e2f76497ad546fe27c5
Sean Lucey <Sean.Lucey@NOAA.gov>
2015-04-08 13:43:50 -0400

    Added a parameter for weight and number so not restricted to BIOMASS and ABUNDANCE

--- Stratmean.r
+++ Stratmean.r
@@ -1,18 +1,16 @@
 #Stratmean.r v.2.0
 #Generate stratified means from survey data
-#6/14
-#v2.0 - changed inputs so can be run on subset of species
-#v1.4 - fixed renaming of columns
-#v1.3 - fixed SE of mean
-#v1.2 - added SE
 #SML
 
-stratmean <- function (x, group.col, strat.col, nsta.col = 'ntows', area.wgt = 'W.h') {
+stratmean <- function (x, group.col, strat.col, nsta.col = 'ntows', area.wgt = 'W.h',
+                       weight = 'BIOMASS', number = 'ABUNDANCE') {
   #x         <- data table with catch, station, and area data
   #group.col <- column of x which the mean is based on (i.e. svspp)
   #strat.col <- column of x with the stratum
   #nsta.col  <- column of x with the number of stations per stratum
   #area.wgt  <- column of x with the stratum weight
+  #weight    <- column of x with the weight values (i.e. biomass)
+  #number    <- column of x with the number of individuals
   
   #-------------------------------------------------------------------------------
   #Required packages
@@ -30,33 +28,33 @@ stratmean <- function (x, group.col, strat.col, nsta.col = 'ntows', area.wgt = '
   #-------------------------------------------------------------------------------
   x <- copy(x)
   
-  setnames(x, c(group.col, strat.col, nsta.col, area.wgt),
-              c('group',   'strat',   'ntows', 'W.h'))
+  setnames(x, c(group.col, strat.col, nsta.col, area.wgt, weight, number),
+              c('group',   'strat',   'ntows', 'W.h',     'BIO',  'N'))
   
   #Fix Na's
-  x[is.na(BIOMASS),   BIOMASS := 0]
-  x[is.na(ABUNDANCE), ABUNDANCE := 0]
+  x[is.na(BIO), BIO := 0]
+  x[is.na(N),   N   := 0]
   
   #Calculate weight per tow and number per tow
   setkey(x, group, strat, YEAR)
   
-  x[, biomass.tow   := sum(BIOMASS)   / ntows, by = key(x)]
-  x[, abundance.tow := sum(ABUNDANCE) / ntows, by = key(x)]
+  x[, biomass.tow   := sum(BIO) / ntows, by = key(x)]
+  x[, abundance.tow := sum(N)   / ntows, by = key(x)]
   
   #Calculated stratified means
   x[, weighted.biomass   := biomass.tow   * W.h]
   x[, weighted.abundance := abundance.tow * W.h]
 
   #Variance - need to account for zero catch
-  x[, n.zero     := ntows - count(BIOMASS), by = key(x)]
+  x[, n.zero     := ntows - count(BIO), by = key(x)]
   
   x[, zero.var.b := n.zero * (0 - biomass.tow)^2]
-  x[, vari.b := (BIOMASS - biomass.tow)^2]
+  x[, vari.b := (BIO - biomass.tow)^2]
   x[, Sh.2.b := (zero.var.b + sum(vari.b)) / (ntows - 1), by = key(x)]
   x[is.nan(Sh.2.b), Sh.2.b := 0]
   
   x[, zero.var.a := n.zero * (0 - abundance.tow)^2]
-  x[, vari.a := (ABUNDANCE - abundance.tow)^2]
+  x[, vari.a := (N - abundance.tow)^2]
   x[, Sh.2.a := (zero.var.a + sum(vari.a)) / (ntows - 1), by = key(x)]
   x[is.nan(Sh.2.a), Sh.2.a := 0]
   
