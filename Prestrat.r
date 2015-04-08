bf306130aaefa0d14bff4cf452918778f5e8da92
Sean Lucey <Sean.Lucey@NOAA.gov>
2015-04-08 10:17:32 -0400

    Changed to only calculate ntows and stratum weights...can now be used with any species groups

--- Prestrat.r
+++ Prestrat.r
@@ -1,7 +1,5 @@
-#Prestrat.r v.1.1
+#Prestrat.r
 #Prepare data for Stratmean.r
-#6/14
-#V1.1 - Added the ability to use different groups than svspp and biomass
 #SML
 
 prestrat <- function (survdat, areas, strat.col, area.col = 'area', sp.col = 'SVSPP') {
@@ -18,9 +16,9 @@ prestrat <- function (survdat, areas, strat.col, area.col = 'area', sp.col = 'SV
   #-------------------------------------------------------------------------------
   #User created functions
   #count occurances
-  count<-function(x){
-    num<-rep(1,length(x))
-    out<-sum(num)
+  count <- function(x){
+    num <- rep(1, length(x))
+    out <- sum(num)
     return(out)
     }
     
@@ -28,56 +26,26 @@ prestrat <- function (survdat, areas, strat.col, area.col = 'area', sp.col = 'SV
   x <- copy(survdat)
   y <- copy(areas)
   
-  setnames(x, c(strat.col, sp.col),
-              c('STRAT',   'SP'))
+  setnames(x, strat.col, 'STRAT')
   setnames(y, c(strat.col, area.col), 
-              c('STRAT', 'S.AREA'))
-              
-  len.var <- c('LENGTH', 'NUMLEN')
-  sta.var <- c('SVVESSEL', 'YEAR', 'SEASON', 'LAT', 'LON', 
-               'DEPTH', 'SURFTEMP', 'SURFSALIN', 'BOTTEMP', 'BOTSALIN')
-  spp.var <- c('SP', 'CATCHSEX', 'ABUNDANCE', 'BIOMASS')
-
-  #Catch data - remove length data
-  setkey(x,
-         CRUISE6,
-         STRAT,
-         STATION,
-         SP,
-         CATCHSEX)
-  
-  catch <- unique(x)
-  catch[, c(len.var, sta.var) := NULL]
-
-  #Merge sexed species
-  setkey(catch,
-         CRUISE6,
-         STRAT,
-         STATION,
-         SP)
-
-  biomass   <- catch[, sum(BIOMASS),      by = key(catch)]
-  abundance <- catch[, sum(ABUNDANCE), by = key(catch)]
-  setnames(biomass,   "V1", "BIOMASS")
-  setnames(abundance, "V1", "ABUNDANCE")
-  catch <- merge(biomass, abundance, by = key(catch))
-               
+              c('STRAT',   'S.AREA'))
+                         
   #Station data - remove catch/length
   setkey(x, CRUISE6, STRAT, STATION)
   stations <- unique(x)
-  stations[, c(len.var, spp.var) := NULL]
+  stations[, c(names(x)[which(!names(x) %in% c('YEAR', 'CRUISE6', 'STRAT', 'STATION'))]) := NULL]
 
   #count stations
   setkey(stations, YEAR, STRAT)
   stations[, ntows := count(STATION), by = key(stations)]
 
   #Merge stations and area
-  stations <- merge(stations, y, by = 'STRAT')
+  stations <- merge(stations, y, by = 'STRAT', all.x = T)
 
   #Calculate stratum weight
   setkey(stations, 'YEAR', 'STRAT')
   strat.year <- unique(stations)
-  strat.year[, which(!names(strat.year) %in% c('YEAR', 'STRAT', 'S.AREA')) := NULL]
+  strat.year[, c('CRUISE6', 'STATION', 'ntows') := NULL]
   strat.year[, W.h := S.AREA / sum(S.AREA, na.rm = T), by = YEAR]
   strat.year[is.na(W.h), W.h := 0]
   strat.year[, S.AREA := NULL]
@@ -86,7 +54,7 @@ prestrat <- function (survdat, areas, strat.col, area.col = 'area', sp.col = 'SV
   stations <- merge(stations, strat.year, by = key(stations))
 
   #Merge catch with station data
-  strat.survdat <- merge(stations, catch, by = c('CRUISE6', 'STRAT', 'STATION'))
+  strat.survdat <- merge(x, stations, by = c('YEAR', 'CRUISE6', 'STRAT', 'STATION'))
   
   setnames(strat.survdat, c('SP',   'STRAT',   'S.AREA'),
                           c(sp.col, strat.col, area.col))
