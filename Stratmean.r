48d18922c9c4abbe91f7374a4e6b1f1124d73c6a
Sean Lucey <Sean.Lucey@NOAA.gov>
2015-04-22 11:11:35 -0400

    changed .S2 to .s2 and added a total number of tows column N in order to calculate confidence intervals

--- Stratmean.r
+++ Stratmean.r
@@ -29,17 +29,23 @@ stratmean <- function (x, group.col, strat.col, nsta.col = 'ntows', area.wgt = '
   x <- copy(x)
   
   setnames(x, c(group.col, strat.col, nsta.col, area.wgt, weight, number),
-              c('group',   'strat',   'ntows', 'W.h',     'BIO',  'N'))
+              c('group',   'strat',   'ntows', 'W.h',     'BIO',  'NUM'))
   
   #Fix Na's
   x[is.na(BIO), BIO := 0]
-  x[is.na(N),   N   := 0]
+  x[is.na(NUM), NUM := 0]
+  
+  #Calculate total number of stations per year
+  setkey(x, strat, YEAR)
+  N <- unique(x)
+  N <- N[, sum(ntows), by = 'YEAR']
+  setnames(N, 'V1', 'N')
   
   #Calculate weight per tow and number per tow
   setkey(x, group, strat, YEAR)
   
   x[, biomass.tow   := sum(BIO) / ntows, by = key(x)]
-  x[, abundance.tow := sum(N)   / ntows, by = key(x)]
+  x[, abundance.tow := sum(NUM) / ntows, by = key(x)]
   
   #Calculated stratified means
   x[, weighted.biomass   := biomass.tow   * W.h]
@@ -54,29 +60,32 @@ stratmean <- function (x, group.col, strat.col, nsta.col = 'ntows', area.wgt = '
   x[is.nan(Sh.2.b), Sh.2.b := 0]
   
   x[, zero.var.a := n.zero * (0 - abundance.tow)^2]
-  x[, vari.a := (N - abundance.tow)^2]
+  x[, vari.a := (NUM - abundance.tow)^2]
   x[, Sh.2.a := (zero.var.a + sum(vari.a)) / (ntows - 1), by = key(x)]
   x[is.nan(Sh.2.a), Sh.2.a := 0]
   
   stratified <- unique(x)
   
-  setkey(stratified, group, YEAR)
+  stratified <- merge(stratified, N, by = 'YEAR')
   
   #Stratified mean  
+  setkey(stratified, group, YEAR)
+  
   stratified[, strat.biomass := sum(weighted.biomass),   by = key(stratified)]
   stratified[, strat.abund   := sum(weighted.abundance), by = key(stratified)]
   
   #Stratified variance
-  stratified[, biomass.S2 := sum(Sh.2.b * W.h) / sum(ntows) + sum((1 - W.h) * Sh.2.b)/sum(ntows)^2, by = key(stratified)]
-  stratified[, abund.S2   := sum(Sh.2.a * W.h) / sum(ntows) + sum((1 - W.h) * Sh.2.a)/sum(ntows)^2, by = key(stratified)]
+  stratified[, biomass.s2 := sum(Sh.2.b * W.h) / N + sum((1 - W.h) * Sh.2.b) / N^2, by = key(stratified)]
+  stratified[, abund.s2   := sum(Sh.2.a * W.h) / N + sum((1 - W.h) * Sh.2.a) / N^2, by = key(stratified)]
   
   #standard error of the means
-  stratified[, biomass.SE := sqrt(biomass.S2) / sqrt(sum(ntows)), by = key(stratified)]
-  stratified[, abund.SE   := sqrt(abund.S2)   / sqrt(sum(ntows)), by = key(stratified)]
+  stratified[, biomass.SE := sqrt(biomass.s2) / sqrt(N), by = key(stratified)]
+  stratified[, abund.SE   := sqrt(abund.s2)   / sqrt(N), by = key(stratified)]
   
   #Delete extra rows/columns
   stratified.means <- unique(stratified)
-  stratified.means <- stratified.means[, list(YEAR, group, strat.biomass, biomass.S2, biomass.SE, strat.abund, abund.S2, abund.SE)]
+  stratified.means <- stratified.means[, list(YEAR, group, N, strat.biomass, biomass.s2, biomass.SE, 
+                                              strat.abund, abund.s2, abund.SE)]
 
   setnames(stratified.means, 'group', group.col)
   
