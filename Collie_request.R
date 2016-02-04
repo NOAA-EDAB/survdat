c970d55166b7f4f216f6361b3fdc1d8fa8515b23
Sean Lucey <Sean.Lucey@NOAA.gov>
2016-02-04 10:46:33 -0500

    request sent

--- Collie_request.R
+++ Collie_request.R
@@ -42,12 +42,13 @@ fall   <- survdat.epu[SEASON == 'FALL', ]
 spring <- survdat.epu[SEASON == 'SPRING']
 
 #Calculate mean/variance per EPU
+#Fall
+#Remove lengths
 setkey(fall, CRUISE6, STRATUM, STATION, SVSPP, CATCHSEX)
 fall <- unique(fall)
 
 #Run stratification prep
-fall.prep <- stratprep(fall,   strat.area, strat.col = 'EPU', area.col = 'Area')
-sp.prep   <- stratprep(spring, strat.area, strat.col = 'EPU', area.col = 'Area')
+fall.prep <- stratprep(fall, strat.area, strat.col = 'EPU', area.col = 'Area')
 
 #Calculate means
 setkey(fall.prep, YEAR, EPU, SVSPP, CATCHSEX)
@@ -55,7 +56,6 @@ fall.prep[, biomass.sum := sum(BIOMASS,   na.rm = T), by = key(fall.prep)]
 fall.prep[, abund.sum   := sum(ABUNDANCE, na.rm = T), by = key(fall.prep)]
 fall.prep[, biomass.tow := biomass.sum / ntows]
 fall.prep[, abund.tow   := abund.sum   / ntows]
-fall.prep[, biomass.var := (zero.var + sum(var)) / (ntows - 1), by = key(fall.prep)]
 
 #Calculate variance
 #Calculate zero tows
@@ -78,17 +78,102 @@ fall.mean <- unique(fall.prep[, list(YEAR, EPU, SVSPP, CATCHSEX,
                                      biomass.tow, biomass.var,
                                      abund.tow,   abund.var)])
 
+#Spring
+#Fall
+setkey(spring, CRUISE6, STRATUM, STATION, SVSPP, CATCHSEX)
+spring <- unique(spring)
 
+#Run stratification prep
+spring.prep <- stratprep(spring, strat.area, strat.col = 'EPU', area.col = 'Area')
+
+#Calculate means
+setkey(spring.prep, YEAR, EPU, SVSPP, CATCHSEX)
+spring.prep[, biomass.sum := sum(BIOMASS,   na.rm = T), by = key(spring.prep)]
+spring.prep[, abund.sum   := sum(ABUNDANCE, na.rm = T), by = key(spring.prep)]
+spring.prep[, biomass.tow := biomass.sum / ntows]
+spring.prep[, abund.tow   := abund.sum   / ntows]
+
+#Calculate variance
+#Calculate zero tows
+spring.prep[, npos := length(unique(paste(STATION, STRATUM))), by = key(spring.prep)]
+spring.prep[, nneg := ntows - npos]
+
+#Biomass Var
+spring.prep[, zero.var    := nneg * (0 - biomass.tow) ^ 2]
+spring.prep[, var         := (BIOMASS - biomass.tow)  ^ 2]
+spring.prep[, biomass.var := (zero.var + sum(var)) / (ntows - 1), 
+          by = key(spring.prep)]
+
+#Abundance Var
+spring.prep[, zero.var.a := nneg * (0 - abund.tow)  ^ 2]
+spring.prep[, var.a      := (ABUNDANCE - abund.tow) ^ 2]
+spring.prep[, abund.var  := (zero.var.a + sum(var.a)) / (ntows - 1), 
+          by = key(spring.prep)]
+
+spring.mean <- unique(spring.prep[, list(YEAR, EPU, SVSPP, CATCHSEX, 
+                                     biomass.tow, biomass.var,
+                                     abund.tow,   abund.var)])
 
 #Calculate total biomass/abundance estimates
-#**Need to add pelagics**
+#Load catchability from EMAX
 load(file.path(data.dir, 'EMAX_groups.RData'))
-q.table <- emax[SVSPP %in% esr.sp, ]
+q.table.fall   <- emax[, list(SVSPP, Fall.q)]
+q.table.spring <- emax[, list(SVSPP, Spring.q)]
+setnames(q.table.fall,   'Fall.q',   'q')
+setnames(q.table.spring, 'Spring.q', 'q')
 
-total.biomass <- sweptarea(GOM.prep, lob.mean, strat.col = 'STRATUM', 
-                           area.col = 'Area')
+#Calculate Areas
+strat.area[, a := 0.0384] #Standard Albatross tow
+strat.area[, A.a := Area / a]
+
+#Expand mean per tow by swept area
+#Fall
+fall.mean <- merge(fall.mean, q.table.fall, all.x = T, by = 'SVSPP')
+fall.mean <- merge(fall.mean, strat.area, by = 'EPU')
+fall.mean[, A.a.q := A.a / q]
+
+fall.mean[, Total.biomass := biomass.tow * A.a.q]
+fall.mean[, Tot.bio.var   := biomass.var * A.a.q ^ 2]
+
+fall.mean[, Total.abundance := round(abund.tow * A.a.q)]
+fall.mean[, Tot.abund.var   := abund.var * A.a.q ^ 2]
+
+fall <- fall.mean[, c('q', 'Area', 'a', 'A.a', 'A.a.q') := NULL]
+
+#Spring
+spring.mean <- merge(spring.mean, q.table.spring, all.x = T, by = 'SVSPP')
+spring.mean <- merge(spring.mean, strat.area, by = 'EPU')
+spring.mean[, A.a.q := A.a / q]
+
+spring.mean[, Total.biomass := biomass.tow * A.a.q]
+spring.mean[, Tot.bio.var   := biomass.var * A.a.q ^ 2]
+
+spring.mean[, Total.abundance := round(abund.tow * A.a.q)]
+spring.mean[, Tot.abund.var   := abund.var * A.a.q ^ 2]
+
+spring <- spring.mean[, c('q', 'Area', 'a', 'A.a', 'A.a.q') := NULL]
 
 #Output results either to a flat .csv file or .RData set
-write.csv(total.biomass, file = paste(out.dir, 'Lobster_GoM.csv',   sep = ''), 
+write.csv(fall, file = file.path(out.dir, 'NEFSC_Fall_Survey.csv'), row.names = F)
+save(     fall, file = file.path(out.dir, 'NEFSC_Fall_Survey.RData'))
+
+write.csv(spring, file = file.path(out.dir, 'NEFSC_Spring_Survey.csv'), 
           row.names = F)
-save(     total.biomass, file = paste(out.dir, 'Lobster_GOM.RData', sep = ''))
+save(     spring, file = file.path(out.dir, 'NEFSC_Spring_Survey.RData'))
+
+#Commercial Data
+data.dir <- '/home/slucey/slucey/EcoAP/Data/Commercial'
+out.dir <- data.dir
+
+load(file.path(data.dir, 'comland_meatwt.RData'))
+
+#Condense data to year/EPU
+setkey(comland, YEAR, EPU, NESPP3)
+landings.epu <- comland[, sum(SPPLIVMT), by = key(comland)]
+setnames(landings.epu, 'V1', 'SPPLIVMT')
+
+write.csv(landings.epu, file = file.path(out.dir, 'Commercial_Landings_by_EPU.csv'),
+          row.names = F)
+save(landings.epu, file = file.path(out.dir, 'Commercial_Landings_by_EPU.RData'))
+
+
