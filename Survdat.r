63326f9f6674142b493fd2d63f82dc7cc1bf53d5
Sean Lucey <Sean.Lucey@NOAA.gov>
2019-07-09 14:59:57 -0400

    Fixed bigelow conversion error for abundance

--- Survdat.r
+++ Survdat.r
@@ -7,22 +7,22 @@
 if(Sys.info()['sysname']=="Windows"){
   data.dir <- "L:\\Rworkspace\\RSurvey"
   out.dir  <- "L:\\EcoAP\\Data\\survey"
-  memory.limit(4000)
 }
 if(Sys.info()['sysname']=="Linux"){
   data.dir <- "/home/slucey/slucey/Rworkspace/RSurvey"
   out.dir  <- "/home/slucey/slucey/EcoAP/Data/survey"
-  uid      <- 'slucey'
-  cat("Oracle Password: ")
-  pwd <- scan(stdin(), character(), n = 1)
 }
+
+uid      <- 'slucey'
+cat("Oracle Password: ")
+pwd <- scan(stdin(), character(), n = 1)
 
 shg.check  <- 'y' # y = use only SHG <=136 or TOGA <= 1324 (>2008)
 raw.check  <- 'n' # y = save data without conversions (survdat.raw), will still 
                   #     save data with conversions (survdat)
 all.season <- 'n' # y = save data with purpose code 10 not just spring/fall 
                   #     (survdat.allseason), will not save survdat regular
-use.SAD    <- 'y' # y = grab data from Survey Analysis Database (SAD) for 
+use.SAD    <- 'n' # y = grab data from Survey Analysis Database (SAD) for 
                   #     assessed species
 #-------------------------------------------------------------------------------
 #Required packages
@@ -44,11 +44,7 @@ sqltext <- function(x){
 
 #-------------------------------------------------------------------------------
 #Begin script
-if(Sys.info()['sysname']=="Windows"){
-  channel <- odbcDriverConnect()
-} else {
-  channel <- odbcConnect('sole', uid, pwd)
-}
+channel <- odbcConnect('sole', uid, pwd)
 
 #Generate cruise list
 if(all.season == 'n'){
@@ -201,35 +197,77 @@ for(i in 1:length(vcf.spp)){
       ABUNDANCE := round(ABUNDANCE * convert[SVSPP == vcf.spp[i], VCF_NUM])]
   }
 
-#Bigelow >2008 Vessel Conversion - need flat files (not on network)
+#Bigelow >2008 Vessel Conversion 
 #Use Bigelow conversions for Pisces as well (PC)
-big.fall   <- as.data.table(read.csv(file.path(data.dir, 
-                                               'bigelow_fall_calibration.csv')))
-big.spring <- as.data.table(read.csv(file.path(data.dir, 
-                                               'bigelow_spring_calibration.csv')))
+#Tables 53-55 from Miller et al. 2010 - number estimators
+big.abund <- data.table(svspp = c(12, 22, 24, 27, 28, 31, 33, 34, 73, 76, 106, 107, 
+                                  109, 121, 135, 136, 141, 143, 145, 149, 155, 164, 
+                                  171, 181, 193, 197, 502, 512, 15, 23, 26, 32, 72,
+                                  74, 77, 78, 102, 103, 104, 105, 108, 131, 163, 301,
+                                  313, 401, 503, 15, 23, 26, 32, 72, 74, 77, 78, 102, 
+                                  103, 104, 105, 108, 131, 163, 301, 313, 401, 503),
+                        season = c(rep('both', 28), rep('spring', 19), rep('fall', 19)),
+                        rho = c(1.161, 4.44, 6.689, 4.384, 3.792, 1.227, 1.323, 1.29,
+                                1.987, 2.235, 2.49, 3.257, 8.249, 1.188, 1.16, 1.134,
+                                3.416, 1.705, 1.577, 1.541, 1.456, 1.802, 4.494, 0.57,
+                                4.575, 7.129,	1.38, 2.309, 1.202, 3.822, 3.08, 2.287,
+                                6.283, 0.972, 3.959, 3.839, 2.074, 3.226, 3.099, 2.347,
+                                3.311, 1.487, 3.56, 1.571, 3.343, 3.965, 2.034, 1.204,
+                                2.609, 9.846, 2, 4.354, 0.243, 2.662, 2.319, 2.16, 
+                                2.405, 2.356, 2.366, 0.2, 0.172, 3.114, 1.586, 2.511,
+                                3.166, 0.84))
 
-bf.spp <- big.fall[pW != 1, svspp]
-for(i in 1:length(bf.spp)){
-  survdat[SVVESSEL %in% c('HB', 'PC') & SEASON == 'FALL' & SVSPP == bf.spp[i],
-      BIOMASS := BIOMASS / big.fall[svspp == bf.spp[i], pW]]
-  }
-bf.spp <- big.fall[pw != 1, svspp]
-for(i in 1:length(bf.spp)){
-  survdat[SVVESSEL %in% c('HB', 'PC') & SEASON == 'FALL' & SVSPP == bf.spp[i],
-      ABUNDANCE := round(ABUNDANCE / big.fall[svspp == bf.spp[i], pw])]
+#Tables 56-58 from Miller et al. 2010 Biomass estimators
+big.bio <- data.table(svspp = c(12, 22, 24, 27, 28, 31, 33, 34, 73, 76, 106, 107, 
+                                109, 121, 135, 136, 141, 143, 145, 149, 155, 164, 
+                                171, 181, 193, 197, 502, 512, 15, 23, 26, 32, 72,
+                                74, 77, 78, 102, 103, 104, 105, 108, 131, 163, 301,
+                                313, 401, 503, 15, 23, 26, 32, 72, 74, 77, 78, 102, 
+                                103, 104, 105, 108, 131, 163, 301, 313, 401, 503),
+                      season = c(rep('both', 28), rep('spring', 19), rep('fall', 19)),
+                      rhoW = c(1.082, 3.661, 6.189, 4.45, 3.626, 1.403, 1.1, 2.12,
+                               1.58, 2.088, 2.086, 3.257, 12.199, 0.868, 0.665, 1.125,
+                               2.827, 1.347, 1.994, 1.535, 1.191, 1.354, 3.259, 0.22,
+                               3.912, 8.062, 1.409, 2.075, 1.166, 3.718, 2.786, 5.394,
+                               4.591, 0.878, 3.712, 3.483, 2.092, 3.066, 3.05, 2.244,
+                               3.069, 2.356, 2.986, 1.272, 3.864, 1.85, 2.861, 1.21, 
+                               2.174, 8.814, 1.95, 4.349, 1.489, 3, 2.405, 1.692,
+                               2.141, 2.151, 2.402, 1.901, 1.808, 2.771, 1.375, 2.479,
+                               3.151, 1.186))
+
+#ID species to convert
+spp.both <- big.abund[season == 'both', svspp]
+spp.seasonal <- big.abund[season == 'fall', svspp]
+
+#Both seasons  
+for(ispp in 1:length(spp.both)){
+  survdat[SVVESSEL %in% c('HB', 'PC') & SVSPP == spp.both[ispp], 
+          BIOMASS := BIOMASS / big.bio[svspp == spp.both[ispp], rhoW]]
+  survdat[SVVESSEL %in% c('HB', 'PC') & SVSPP == spp.both[ispp],
+          ABUNDANCE := round(ABUNDANCE / big.abund[svspp == spp.both[ispp], rho])]
+}
+#Seperate calibration factors by season
+for(ispp in 1:length(spp.seasonal)){
+  survdat[SVVESSEL %in% c('HB', 'PC') & SEASON == 'SPRING' & SVSPP == spp.seasonal[ispp],
+          BIOMASS := BIOMASS / big.bio[svspp == spp.seasonal[ispp] & season == 'spring' , 
+                                       rhoW]]
+  
+  survdat[SVVESSEL %in% c('HB', 'PC') & SEASON == 'FALL' & SVSPP == spp.seasonal[ispp],
+          BIOMASS := BIOMASS / big.bio[svspp == spp.seasonal[ispp] & season == 'fall' , 
+                                       rhoW]]
+  
+  survdat[SVVESSEL %in% c('HB', 'PC') & SEASON == 'SPRING' & SVSPP == spp.seasonal[ispp],
+          ABUNDANCE := round(ABUNDANCE / big.abund[svspp == spp.seasonal[ispp] & 
+                                                     season == 'spring' , rho])]
+  
+  survdat[SVVESSEL %in% c('HB', 'PC') & SEASON == 'FALL' & SVSPP == spp.seasonal[ispp],
+          ABUNDANCE := round(ABUNDANCE / big.abund[svspp == spp.seasonal[ispp] & 
+                                                     season == 'fall' , rho])]
   }
 
-bs.spp <- big.spring[pW != 1, svspp]
-for(i in 1:length(bs.spp)){
-  survdat[SVVESSEL %in% c('HB', 'PC') & SEASON == 'SPRING' & SVSPP == bs.spp[i],
-      BIOMASS := BIOMASS / big.spring[svspp == bs.spp[i], pW]]
-  }
-bs.spp <- big.spring[pw != 1, svspp]
-for(i in 1:length(bs.spp)){
-  survdat[SVVESSEL %in% c('HB', 'PC') & SEASON == 'SPRING' & SVSPP == bs.spp[i],
-      ABUNDANCE := round(ABUNDANCE / big.spring[svspp == bs.spp[i], pw])]
-  }
 
+
+#Need to update...no longer SAD now ADIOS.  Table structure has changed
 if(use.SAD == 'y'){
   sad.qry <- "select svspp, cruise6, stratum, tow, station, sex as catchsex,
              catch_wt_B_cal, catch_no_B_cal, length, length_no_B_cal
