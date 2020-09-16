fa3cbe79456bfe44e84b216f9fe780a7fbe643ff
andybeet <22455149+andybeet@users.noreply.github.com>
2020-09-16 15:41:58 -0400

    package documentation

---
+++ R/survdat.R
@@ -0,0 +1,17 @@
+#'survdat: Pull and process Survey data
+#'
+#'For use in multispecies projects.
+#'
+#'\itemize{
+#'\item pulls data from svdbs
+#'}
+#'
+#'@section References:
+#'
+#'
+#'To learn more about using \code{survdat}, start with the vignette: \code{browseVignettes(package="survdat")} or click the index below
+#'
+#'
+#' @docType package
+#' @name survdat
+NULL
