a1959f92236166f2f0bc183d5c041d8612563243
andybeet <22455149+andybeet@users.noreply.github.com>
2020-09-25 16:17:18 -0400

    added unit conversion in get_area

--- R/get_area.R
+++ R/get_area.R
@@ -9,18 +9,20 @@
 #'@param strata.col Character String. Column name from stratum that contains the strata designations.
 #'@param crs Character string. Specifiy the coordinate refernce system.
 #'
-#'@return Returns a table of areas in square kilometers.
+#'@return Returns a data.table of areas in square kilometers.
 #'
 #'@export
 
 
 get_area <- function(stratum, col.name="STRATA", crs="+proj=lcc +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-72 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0 "){
 
+  Area <- units::set_units(sf::st_area(stratum,crs),km^2)
+
   strata <- stratum %>%
     as.data.frame() %>%
     dplyr::select(col.name) %>%
     dplyr::rename(STRATA = col.name) %>%
-    cbind(.,Area = sf::st_area(stratum,crs)) %>%
+    cbind(.,Area) %>%
     data.table::as.data.table()
 
   return(strata)
