616a2a3f9ee341a33d073de36d7b3df1d331a0f3
andybeet <22455149+andybeet@users.noreply.github.com>
2020-10-06 17:01:41 -0400

    updated general package info

--- R/survdat.R
+++ R/survdat.R
@@ -1,9 +1,12 @@
 #'survdat: Pull and process Survey data
 #'
-#'For use in multispecies projects.
+#'Tools to aid in extraction of survey data from the svdbs database
 #'
 #'\itemize{
-#'\item pulls data from svdbs
+#'\item Pulls data from svdbs
+#'\item Applies corrections to the data based on changes in gear and ships
+#'\item Partitions the data based on user supplied geospatial data
+#'\item Estimates biomass of species
 #'}
 #'
 #'@section References:
