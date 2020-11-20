1a74566559eba383abe290b346b455f192be92f5
Sean Lucey <Sean.Lucey@NOAA.gov>
2020-11-19 20:44:03 -0500

    fixed bug of renaming a strat column as stratum even if not the original stratum

--- R/strat_prep.R
+++ R/strat_prep.R
@@ -58,7 +58,7 @@ strat_prep <- function (survdata, areas, strat.col) {
   strat.survdat <- merge(x, stations, by = c('YEAR', 'CRUISE6', 'STRAT', 'STATION'))
 
   data.table::setnames(strat.survdat, c('STRAT', 'S.AREA'),
-           c("STRATUM", "Area"))
+           c(areaDescription, "Area"))
 
   if(strat.col != 'STRATA'){
     data.table::setnames(strat.survdat, c('STATION', 'ORIGSTATION'), c('STATION2', 'STATION'))
