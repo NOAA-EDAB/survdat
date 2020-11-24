88e72febb071e44b60b2cffe7c18b22bd66e07db
Sean Lucey <Sean.Lucey@NOAA.gov>
2020-11-24 16:24:44 -0500

    switch default polygon from NA to NULL to avoid warning message

--- R/swept_area_biomass.r
+++ R/swept_area_biomass.r
@@ -38,14 +38,14 @@
 #' @export
 
 
-swept_area_biomass <- function(data, areaPolygon = NA, areaDescription, 
+swept_area_biomass <- function(data, areaPolygon = NULL, areaDescription, 
                                filterByArea = "all", filterBySeason, species = "all", 
                                crs = "+proj=lcc +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-72 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0", 
                                merge.sex = T, q = NULL, a =0.0384, 
                                tidy = F) {
 
   #Use original stratified design and built-in shapefile
-  if(is.na(areaPolygon)){
+  if(is.null(areaPolygon)){
     areaPolygon <- sf::st_read(dsn = system.file("extdata", "strata.shp", 
                                                  package = "survdat"), quiet = T)
     data$STRATUM <- as.numeric(data$STRATUM)
