baa4a7a37b8df75d77e8383775d3e6dad387bf12
Sean Lucey <Sean.Lucey@NOAA.gov>
2020-11-25 13:06:58 -0500

    Start of new function based off swept_area_biomass

---
+++ R/calc_stratified_mean.R
@@ -0,0 +1,104 @@
+#' Calculate swept area biomass
+#'
+#' Calculates the biomass
+#'
+#' @param data Data frame. Survey data pull from svdbs with applied conversion factors
+#' @param areaPolygon sf object. Defining the areas by which to estimate biomass.
+#' @param areaDescription Character string. The name of the column in areaPolygon that describes the area (eg. "EPU","STRATA")
+#' @param filterByArea Character Vector. Vecor of area names. Names found in the \code{areaPolygon} in \code{areaDescription} Column. Biomass estimates will be aggregated over all areas specified in \code{filterByArea} (Default = "all")
+#' @param filterBySeason Character Vector. Vector of season names. Names found in the SEASON column of \code{data}
+#' @param species Character Vector. Vector of species SVSPP codes.
+#' @param merge.sex Boolean. Merge sexed species such as dogfish. (Default = T)
+#' @param poststrat Boolean. Indicating whether the original strata design was
+#'used or not.  Changes the calculation for variance. (Default = F)
+#' @param q Table of survey catchability with a column of group names and a column of
+#'catchabilities.  If not provided, assumes a q of 1 for each group (Minimum swept area
+#'estimates).
+#' @param a The average swept area of the trawl.  Default value is the swept area of a
+#'standard NOAA Ship Albatross IV tow.
+#' @param tidy Boolean. Return output in long format (Default = F)
+#'
+#' @return
+#'
+#'
+#' @examples
+#' \dontrun{
+#' # pull data and apply conversion corrections
+#' data <- survdatData
+#'
+#' # read in "strata" shapefile
+#' area <- sf::st_read(dsn = system.file("extdata","strata.shp",package="survdat"),quiet=T)
+#'
+#' # estimate swept area biomass in the SPRING in STRATA = 1220, 1240, 1260:1290,1360:1400 for species = 300:310
+#' swa <-swept_area_biomass(data=data, areaPolygon = area, areaDescription="STRATA", filterByArea=c(1220, 1240, 1260:1290,1360:1400),filterBySeason = "SPRING", species=c(300:310))
+#'
+#' }
+#'
+#'
+#' @export
+
+
+calc_stratified_mean <- function(surveyData, areaPolygon = NULL, 
+                                 areaDescription = NULL, filterByArea = "all", 
+                                 filterBySeason, groupDescription = "SVSPP", 
+                                 filterByGroup = "all", filterBySex = "all", 
+                                 q = NULL, a =0.0384, tidy = F) {
+
+  # Use original stratified design and built-in shapefile
+  if(is.null(areaPolygon)){
+    areaPolygon <- sf::st_read(dsn = system.file("extdata", "strata.shp", 
+                                                 package = "survdat"), quiet = T)
+    areaDescription <- "STRATA"
+    poststratFlag <- F
+  } else {poststratFlag <- T} #Not using original stratification
+  
+  # Calculate area of the polygons
+  polygon.area <- survdat::get_area(areaPolygon, areaDescription)
+  
+  # post stratify if necessary
+  if(poststratFlag){
+    message("Post stratifying ...")
+    surveyData <- survdat::post_strat(surveyData, areaPolygon, areaDescription)
+  } else {
+    surveyData <- surveyData[, areaDescription := STRATUM]
+    data.table::setnames(surveydata, 'areaDescription', areaDescription)
+    }
+
+  # check to create all areas
+  if (length(filterByArea)==1) {
+    if (filterByArea == "all") {
+      filterByArea <- areaPolygon %>%
+        sf::st_drop_geometry() %>%
+        dplyr::select(areaDescription) %>%
+        unlist() %>%
+        unique()
+      if (is.factor(filterByArea)){ # convert to levels
+        filterByArea <- levels(filterByArea)
+      }
+    }
+  }
+  print(filterByArea)
+
+  filteredData <- surveyData[SEASON == filterBySeason & get(areaDescription) %in% filterByArea, ]
+
+  #Coded through to here...
+  #Run stratification prep
+  message("Prepping  ...")
+  survdatPrep <- survdat::strat_prep(filteredData, strat.area, strat.col = areaDescription)
+
+  #Calculate stratified mean
+  message("Calculating Stratified Mean  ...")
+  stratGroupMean <- survdat::strat_mean(survdatPrep, groups = species, 
+                                        group.col = 'SVSPP', strat.col = areaDescription,
+                                        poststrat = poststrat, merge.sex = merge.sex)
+
+  #Calculate total biomass/abundance estimates
+  message("Calculating Swept Area Estimate  ...")
+  total.biomass <- survdat::swept_area(survdat=survdatPrep, stratmean=stratGroupMean,
+                                       q = q, strat.col = areaDescription)
+
+  # create tidy data
+
+  return(total.biomass)
+}
+
