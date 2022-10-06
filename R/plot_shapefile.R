f6c4b0c11872749b7735f295308d79665b5cf2c8
andybeet <22455149+andybeet@users.noreply.github.com>
2022-10-06 17:00:27 -0400

    aded filtering to plot_shapefile function

--- R/plot_shapefile.R
+++ R/plot_shapefile.R
@@ -2,10 +2,11 @@
 #'
 #'@param polygons sf object. The polygons (shapefile)
 #'@param crs Character string. Defines the coordinate reference system for projection. Default = 4269 (NAD83)
+#'@param filterPolygons Numeric Vector. (Default = NULL) Set of polygons to plot in a differnt color
 #'
 #'@return A figure
 #'
-#'#'@family plotting
+#'@family plotting
 #'
 #'@examples
 #'\dontrun{
@@ -17,14 +18,24 @@
 #'
 #'@export
 
-plot_shapefile <- function(polygons,crs=4269) {
+plot_shapefile <- function(polygons,crs=4269, filterPolygons=NULL) {
 
   # transform crs
   polygons <- sf::st_transform(polygons,crs)
 
-  # plot shapefile
-  p <- ggplot2::ggplot() +
-    ggplot2::geom_sf(data=polygons,color = "Grey",alpha = .5)
+
+  if (is.null(filterPolygons)) {
+    p <- ggplot2::ggplot() +
+      ggplot2::geom_sf(data=polygons,color = "Grey",alpha = .5)
+
+  } else {
+
+    # plot shapefile
+    p <- ggplot2::ggplot() +
+      ggplot2::geom_sf(data=polygons,color = "Grey",alpha = .5) +
+      ggplot2::geom_sf(data=polygons %>% dplyr::filter(STRATA %in% filterPolygons),
+                       color = "blue",alpha = .5)
+  }
 
   print(p)
   #return(p)
