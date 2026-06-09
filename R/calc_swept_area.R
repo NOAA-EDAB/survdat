248b37d7ed9f76e9040cc662334fb3438856242a
Robert Gamble <Robert.Gamble@noaa.gov>
2026-06-09 15:28:47 -0400

    Apply suggestion from @andybeet
    Co-authored-by: Andy Beet <22455149+andybeet@users.noreply.github.com>

--- R/calc_swept_area.R
+++ R/calc_swept_area.R
@@ -7,7 +7,7 @@
 #'  to \code{groupDescription} and a column of catchabilities.  If NULL, assumes
 #'  a \code{q} of 1 for each \code{groupDescription} (Minimum swept area estimates).
 #' @param a Numeric. The average swept area of the trawl.  Default value is the
-#'  swept area of a standard NOAA Ship Albatross IV tow.(NEFSC, 2006)
+#'  swept area of a standard NOAA Ship Albatross IV tow.(\href{https://repository.library.noaa.gov/view/noaa/25243}{NEFSC, 2006})
 #'
 #' @section Source:
 #'
