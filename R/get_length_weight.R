b929e7ba8fb2e6e1c8b40847c5709cca26fdb23e
Stephanie Owen <1626609313113305@mil>
2026-06-10 10:04:16 -0400

    docs: get_length_weight - added example function pull to documentation (under 'dontrun')

--- R/get_length_weight.R
+++ R/get_length_weight.R
@@ -14,7 +14,14 @@
 #'
 #'@family helper
 #'
+#' @examples
+#' \dontrun{
+#' channel <- dbutils::connect_to_database(server="serverName",uid="userName")
+#' get_length_weight(channel = channel)
+#'}
+#'
 #'@export
+
 
 get_length_weight <- function(channel) {
   #Grab survey length/weight coefficients
