57ce21e8d79d049b9b94468166c2b8b39fddc4e0
andybeet <22455149+andybeet@users.noreply.github.com>
2026-06-30 12:03:13 -0400

    fix(units): changed units to conform to output type

--- R/calc_stratified_mean.R
+++ R/calc_stratified_mean.R
@@ -129,7 +129,7 @@ calc_stratified_mean <- function(
     tidyData[variable == 'biomass.SE', units := 'kg tow^-1']
     tidyData[variable == 'strat.abund', units := 'number tow^-1']
     tidyData[variable == 'abund.var', units := '(numbers tow^-1)^2']
-    tidyData[variable == 'abund.SE', units := 'kg tow^-1']
+    tidyData[variable == 'abund.SE', units := 'number tow^-1']
     stratmeanData <- tidyData
   }
 
