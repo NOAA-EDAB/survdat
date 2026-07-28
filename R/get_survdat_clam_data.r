3341921a89d2f3a68e57da854805f0a0c49a4754
Maxwell Grezlik <maxwell.grezlik@noaa.gov>
2026-07-28 10:28:41 -0400

    fixing capitalization of variables

--- R/get_survdat_clam_data.r
+++ R/get_survdat_clam_data.r
@@ -1,67 +1,16 @@
 #' Extracts Clam data from Survey Database
 #'
 #'Connects to svdbs and pulls Clam & Quahog data from MSTR_CRUISE, UNION_FSCS_SVCAT, UNION_FSCS_SVLEN, UNION_FSCS_SVSTA.
-#'Pulls from Cruises with purpose code = 50. (See \code{\url{get_cruise_purpose}}). Data are assigned to one of 7 regions
-#'('SVA', 'DMV', 'SNJ', 'NNJ', 'LI', 'SNE', 'GB') and length-to-meat weight conversions applied
+#'Pulls from Cruises with purpose code = 50. (See \code{\url{get_cruise_purpose}}). Data are assigned to one of 2 new regions
+#'('South', 'GBK') and length-to-meat weight conversions applied.
 #'
 #' @param channel an Object inherited from \link[DBI]{DBIConnection-class}. This object is used to communicate with the database engine. (see \code{\link[dbutils]{connect_to_database}})
 #' @param shg.check Boolean. use only SHG <=136 or TOGA <= 1324 (>2008). (Default = T)
 #' @param clam.only Boolean. T = grab only Atl. surfclam (403) and ocean quahog (409)
 #' @param tidy Boolean. Return output in long format (Default = F)
-#' @param assignRegionWeights Boolean. Assign Strata to Regions and then apply length weight coefficients. Currently hard coded for Survey strata prior to 2017. (Default = T).
+#' @param assignRegionWeights Boolean. Assign Strata to Regions and then apply length weight coefficients. (Default = T).
 #'
 #' @return A list containing a Data frame (data.table) (n x 21) and a list of SQL queries used to pull the data, the date of the pull, and the call expression
-#' Each row of the data.table represents the number at length of a species on a specific tow along with physical attributes of the tow.
-#'
-#' The data frame (Descriptions taken from NEFSC Data dictionary)
-#'
-#' \item{clam.region}{One of 7 identified Regions, 'SVA', 'DMV', 'SNJ', 'NNJ', 'LI', 'SNE', 'GB'}
-#' \item{CRUISE6}{Code uniquely identifying cruise. The first four digits indicate the year and the last two digit uniquely identify the cruise within the year. The 5th byte signifies cruises other than groundfish: Shrimp survey = 7 (i.e. 201470), State of Massachusetts survey = 9 (i.e. 201491), Food habits = 5 (i.e.199554)}
-#' \item{STATION}{Unique sequential order in which stations have been completed. Hangups and short tows each receive a non-repeated consecutive number.}
-#' \item{STRATUM}{	A predefined area where a net dredge, or other piece of gear was deployed. Code consists of 2 parts: Stratum group code number (2 bytes) and stratum number (3 bytes). Stratum group refers to if area fished is inshore or offshore North or South of Cape Hatteras or the type of cruise (shellfish, State of MA, offshore deepwater). The stratum number (third and fourth digits of code) refers to area defined by depth zone. See SVDBS.SVMSTRATA. The fifth digit of the code increases the length of the stratum number for revised strata after the Hague Line was established. Stratum group code: 01 = Trawl, offshore north of Hatteras; 02 = BIOM; 03 = Trawl, inshore north of Hatteras; 04 = Shrimp; 05 = Scotian shelf; 06 = Shellfish; 07 = Trawl, inshore south of Hatteras; 08 = Trawl, Offshore south of Hatteras; 09 = MA DMF; 99 = Offshore deepwater (outside the stratified area). A change in Bottom Trawl Stratum for the Gulf of Maine-Bay of Fundy has been in effect since Spring 1987, and may be summarized as follows: Previous strata: 01350; Present strata: 01351, 01352.}
-#' \item{SVSPP}{A standard code which represents a species caught in a trawl or dredge. Refer to the SVDBS.SVSPECIES_LIST}
-#' \item{CATCHSEX}{Code used to identify species that are sexed at the catch level. See SVDBS.SEX_CODES}
-#' \item{SVVESSEL}{Standard two character code for a survey vessel. Refer to SVDBS.SV_VESSEL}
-#' \item{YEAR}{	Year in which cruise was conducted.}
-#' \item{LAT}{Beginning latitude of tow in decimal degrees.(DECDEG_BEGLAT)}
-#' \item{LON}{Beginning longitude of tow in decimal degrees.(DECDEG_BEGLON)}
-#' \item{DEPTH}{	A four digit number recording the average depth, to the nearest meter, during a survey gear deployment.(AVGDEPTH)}
-#' \item{SURFTEMP}{Surface temperature of water (degrees Celcius).}
-#' \item{SURFSALIN}{Salinity at water surface in practical salinity units (PSU).}
-#' \item{BOTTEMP}{Bottom temperature (degrees Celsius).}
-#' \item{BOTSALIN}{Bottom salinity in Practical Salinity Units (PSU).}
-#' \item{ABUNDANCE}{Expanded number of individuals of a species caught at a given station.(EXPCATCHNUM)}
-#' \item{BIOMASS}{Expanded catch weight of a species caught at a given station. (EXPCATCHWT)}
-#' \item{LENGTH}{Measured length of species in centimeters (cm). Measure method differs by species.}
-#' \item{NUMLEN}{Expanded number of specimens at a given length.(EXPNUMLEN)}
-#' \item{BIOMASS.MW}{Meat weight of catch based on LENGTH and NUMLEN of clams, conversion factors hard coded}
-#'
-#'
-#' The list of sql statements:
-#'
-#' \item{cruise}{Select unique list of cruises. Table = MSTR_CRUISE}
-#' \item{station}{Select unique set of stations from result of \code{cruise}. Table = UNION_FSCS_SVSTA}
-#' \item{catch}{Select species abundance and biomass data from result of \code{station}. Table = UNION_FSCS_SVCAT}
-#' \item{length}{Select Lengths of species found in \code{catch}. Table = UNION_FSCS_SVLEN}
-#'
-#' The date:
-#'
-#'  \item{pullDate}{The date the data was pulled from the database}
-#'
-#' The expression:
-#'
-#' \item{functionCall}{The call used to create the data pul}
-#'
-#'
-#'@family survdat
-#'
-#'@examples
-#'\dontrun{
-#'# Recommended use:
-#'channel <- dbutils::connect_to_database("serverName","userName")
-#'get_survdat_clam_data(channel)
-#'
-#'}
 #'
 #'@export
 
@@ -69,191 +18,234 @@
 #User parameters
 
 get_survdat_clam_data <- function(
-  channel,
-  shg.check = T,
-  clam.only = T,
-  tidy = F,
-  assignRegionWeights = T
+    channel,
+    shg.check = T,
+    clam.only = T,
+    tidy = F,
+    assignRegionWeights = T
 ) {
   call <- capture_function_call()
-
+  
   #Generate cruise list
-  #V1.2 - remove surveys prior to 1982 due to difference in seasons/gear
   cruise.qry <- "select unique year, cruise6, svvessel
                  from svdbs.mstr_cruise
                  where purpose_code = 50
                  and year >= 1982
                  order by year, cruise6"
-
+  
   cruise <- data.table::as.data.table(DBI::dbGetQuery(channel, cruise.qry))
   cruise <- na.omit(cruise)
   data.table::setkey(cruise, CRUISE6, SVVESSEL)
-
+  
   #Use cruise codes to select other data
   cruise6 <- sqltext(cruise$CRUISE6)
-
+  
   #Station data
   if (shg.check == T) {
     station.qry <- paste(
       "select unique cruise6, svvessel, station, stratum, decdeg_beglat as lat, decdeg_beglon as lon,
                    avgdepth as depth, surftemp, surfsalin, bottemp, botsalin
                    from svdbs.Union_fscs_svsta
-                   where cruise6 in (",
-      cruise6,
-      ")
+                   where cruise6 in (", cruise6, ")
                    and SHG <= 136
-                   order by cruise6, station",
-      sep = ''
+                   order by cruise6, station", sep = ''
     )
   } else {
     station.qry <- paste(
       "select unique cruise6, svvessel, station, stratum, decdeg_beglat as lat, decdeg_beglon as lon,
                    avgdepth as depth, surftemp, surfsalin, bottemp, botsalin
                    from svdbs.UNION_FSCS_SVSTA
-                   where cruise6 in (",
-      cruise6,
-      ")
-                   order by cruise6, station",
-      sep = ''
+                   where cruise6 in (", cruise6, ")
+                   order by cruise6, station", sep = ''
     )
   }
-
+  
   station <- data.table::as.data.table(DBI::dbGetQuery(channel, station.qry))
   data.table::setkey(station, CRUISE6, SVVESSEL)
-
+  
   #merge cruise and station
   clamdat <- base::merge(cruise, station)
-
+  
   #Catch data
   if (clam.only == T) {
     catch.qry <- paste(
       "select cruise6, station, stratum, svspp, catchsex, expcatchnum as abundance, expcatchwt as biomass
                  from svdbs.UNION_FSCS_SVCAT
-                 where cruise6 in (",
-      cruise6,
-      ")
+                 where cruise6 in (", cruise6, ")
                  and svspp in ('403', '409')
-                 order by cruise6, station, svspp",
-      sep = ''
+                 order by cruise6, station, svspp", sep = ''
     )
   } else {
     catch.qry <- paste(
       "select cruise6, station, stratum, svspp, catchsex, expcatchnum as abundance, expcatchwt as biomass
                  from svdbs.UNION_FSCS_SVCAT
-                 where cruise6 in (",
-      cruise6,
-      ")
-                 order by cruise6, station, svspp",
-      sep = ''
+                 where cruise6 in (", cruise6, ")
+                 order by cruise6, station, svspp", sep = ''
     )
   }
-
+  
   catch <- data.table::as.data.table(DBI::dbGetQuery(channel, catch.qry))
   data.table::setkey(catch, CRUISE6, STATION, STRATUM)
-
+  
   #merge with clamdat
   data.table::setkey(clamdat, CRUISE6, STATION, STRATUM)
   clamdat <- base::merge(clamdat, catch, all.x = T)
-
+  
   #Length data
   if (clam.only == T) {
     length.qry <- paste(
       "select cruise6, station, stratum, svspp, catchsex, length, expnumlen as numlen
                   from svdbs.UNION_FSCS_SVLEN
-                  where cruise6 in (",
-      cruise6,
-      ")
+                  where cruise6 in (", cruise6, ")
                   and svspp in ('403', '409')
-                  order by cruise6, station, svspp, length",
-      sep = ''
+                  order by cruise6, station, svspp, length", sep = ''
     )
   } else {
     length.qry <- paste(
       "select cruise6, station, stratum, svspp, catchsex, length, expnumlen as numlen
                   from svdbs.UNION_FSCS_SVLEN
-                  where cruise6 in (",
-      cruise6,
-      ")
-                  order by cruise6, station, svspp, length",
-      sep = ''
+                  where cruise6 in (", cruise6, ")
+                  order by cruise6, station, svspp, length", sep = ''
     )
   }
-
+  
   len <- data.table::as.data.table(DBI::dbGetQuery(channel, length.qry))
   data.table::setkey(len, CRUISE6, STATION, STRATUM, SVSPP, CATCHSEX)
-
+  
   #merge with clamdat
   data.table::setkey(clamdat, CRUISE6, STATION, STRATUM, SVSPP, CATCHSEX)
   clamdat <- base::merge(clamdat, len, all.x = T)
-
+  
   clamdat[, STRATUM := as.numeric(STRATUM)]
-
+  
   if (assignRegionWeights) {
-    #Assign clam regions
-    regions <- c('SVA', 'DMV', 'SNJ', 'NNJ', 'LI', 'SNE', 'GB')
-    SVA <- c(6010:6080, 6800, 6810)
-    DMV <- c(6090:6160, 6820:6860)
-    SNJ <- c(6170:6200, 6870)
-    NNJ <- c(6210:6280, 6880:6900)
-    LI <- c(6290:6360, 6910:6930)
-    SNE <- c(6370:6520, 6940:6960)
-    GB <- c(6530:6740)
-
-    clamdat[, clam.region := factor(NA, levels = regions)]
-    for (i in 1:length(regions)) {
-      clamdat[STRATUM %in% get(regions[i]), clam.region := regions[i]]
-    }
-
-    #shell length-to-meat weight conversion coefficients (OQ NEFSC 2004, SC NEFSC 2003)
+    
+    # 1. Clean the base stratum for geometric splits (remove 6 and 0 prefix)
+    clamdat[, calc_strat := sub("^0?60?", "", as.character(STRATUM))]
+    clamdat[, sv_year := floor(as.numeric(CRUISE6) / 100)]
+    
+    # 2. Geometric Stratum Splits
+    clamdat[calc_strat == '47', 
+            calc_strat := data.table::fifelse(((LON - 69.23) * (41 - 40) - (LAT - 40) * (69.03 - 69.23)) > 0, '471', '472')]
+    
+    clamdat[calc_strat == '73', 
+            calc_strat := data.table::fifelse(((LON - 66.8) * (41.9 - 41.35) - (LAT - 41.35) * (67.5 - 66.8)) > 0, '73', '74')]
+    
+    # Hudson Canyon Quahog Splits
+    clamdat[SVSPP == 409 & calc_strat %in% c('25', '26') & LAT >= 39.3 & LAT <= 40.2 & 
+              (((LON - 72) * (40.2 - 39.3) - (LAT - 39.3) * (73.75 - 72)) < 0), 
+            calc_strat := data.table::fifelse(calc_strat == '26', '30', '29')]
+    
+    clamdat[SVSPP == 409 & calc_strat %in% c('31', '32') & 
+              (((LON - 72) * (40.2 - 39.3) - (LAT - 39.3) * (73.75 - 72)) < 0), 
+            calc_strat := data.table::fifelse(calc_strat == '31', '27', '28')]
+    
+    clamdat[SVSPP == 409 & calc_strat %in% c('25', '26') & LAT >= 40.2 & LAT <= 40.25 & 
+              (((LON - 73.75) * (40.25 - 40.2) - (LAT - 40.25) * (73.775 - 73.75)) < 0), 
+            calc_strat := data.table::fifelse(calc_strat == '25', '29', '30')]
+    
+    clamdat[SVSPP == 409 & calc_strat %in% c('25', '26') & LAT >= 40.25 & LAT <= 40.5 & 
+              (((LON - 73.775) * (40.5 - 40.25) - (LAT - 40.25) * (73.825 - 73.775)) < 0), 
+            calc_strat := data.table::fifelse(calc_strat == '25', '29', '30')]
+    
+    # Southernmost Quahog Splits
+    clamdat[SVSPP == 409 & calc_strat == '17' & 
+              (((LON - 74.29) * (38.6 - 38.94) - (LAT - 38.94) * (74.57 - 74.29)) < 0), 
+            calc_strat := '0']
+    
+    clamdat[SVSPP == 409 & calc_strat == '13' & LAT >= 38.41 & 
+              (((LON - 74.57) * (38.41 - 38.6) - (LAT - 38.6) * (74.64 - 74.57)) < 0), 
+            calc_strat := '0']
+    
+    clamdat[SVSPP == 409 & calc_strat == '13' & LAT >= 38.15 & LAT <= 38.41 & 
+              (((LON - 74.64) * (38.15 - 38.41) - (LAT - 38.41) * (74.67 - 74.64)) < 0), 
+            calc_strat := '0']
+    
+    clamdat[SVSPP == 409 & calc_strat == '13' & LAT <= 38.15 & 
+              (((LON - 74.67) * (37.83 - 38.15) - (LAT - 38.15) * (74.87 - 74.67)) < 0), 
+            calc_strat := '0']
+    
+    # 3. Assign New Strata for Pre-2018
+    clamdat[, new_stratum := calc_strat] 
+    
+    # Surfclams Pre-2018
+    clamdat[SVSPP == 403 & sv_year < 2018, new_stratum := data.table::fcase(
+      calc_strat %in% c('05','09','81'), "1S",
+      calc_strat %in% c('84', '85', '86', '87'), "2S",
+      calc_strat %in% c('13', '17', '21', '25', '29'), "3S",
+      calc_strat %in% c('10', '14', '18', '22'), "4S",
+      calc_strat %in% c('88', '89', '90', '91', '92', '93'), "5S",
+      calc_strat %in% c('45', '46', '95', '96'), "6S",
+      calc_strat %in% c('53', '54'), "7S",
+      calc_strat %in% c('67', '69', '70'), "8S",
+      calc_strat %in% c('57', '58', '59', '60'), "9S",
+      calc_strat %in% c('65', '66'), "10S",
+      calc_strat %in% c('68', '72', '73'), "11S",
+      calc_strat %in% c('71', '74'), "12S",
+      default = "0"
+    )]
+    
+    # Quahogs Pre-2018
+    clamdat[SVSPP == 409 & sv_year < 2018, new_stratum := data.table::fcase(
+      calc_strat %in% c('10', '11', '12', '14', '15', '16', '18', '19', '20'), "1Q",
+      calc_strat %in% c('13', '17', '21', '22', '25', '26'), "2Q",
+      calc_strat %in% c('23', '24', '27', '28', '31', '32', '35', '36'), "3Q",
+      calc_strat %in% c('29', '30', '33', '34'), "4Q",
+      calc_strat %in% c('92', '93', '94', '95', '37', '41'), "5Q",
+      calc_strat %in% c('38', '39', '40', '46', '471', '48'), "6Q",
+      calc_strat %in% c('53','54','55','56','472','56'), "7Q",
+      calc_strat %in% c('70'), "8Q",
+      calc_strat %in% c('57','58','59','60'), "9Q",
+      calc_strat %in% c('65','66'), "10Q",
+      calc_strat %in% c('74'), "11Q",
+      calc_strat %in% c('61','62'), "12Q",
+      default = "0"
+    )]
+    
+    # Ensure DEPTH is capitalized here as well
+    clamdat[!is.na(DEPTH) & DEPTH > 80, new_stratum := '0']
+    
+    # 4. Map to Assessment Regions (South vs GBK)
+    south_strata <- c("1S","2S","3S","4S","5S","6S","1Q","2Q","3Q","4Q","5Q","6Q")
+    clamdat[, clam.region := data.table::fifelse(new_stratum %in% south_strata, "South", "GBK")]
+    
+    # Clean up intermediate geometric columns
+    clamdat[, c('calc_strat', 'sv_year') := NULL]
+    
+    # 5. Apply Meat Weight Coefficients
+    # TODO: Fill in the NA values with the new biological parameters for South/GBK
     coeff <- data.table::data.table(
-      clam.region = c('SVA', 'DMV', 'SNJ', 'NNJ', 'LI', 'SNE', 'GB'),
-      oq.a = c(
-        -9.04231,
-        -9.04231,
-        -9.84718,
-        -9.84718,
-        -9.23365,
-        -9.12428,
-        -8.96907
-      ),
-      oq.b = c(
-        2.787987,
-        2.787987,
-        2.94954,
-        2.94954,
-        2.822474,
-        2.774989,
-        2.767282
-      ),
-      sc.a = c(-7.0583, -9.48913, -9.3121, -9.3121, -7.9837, -7.9837, -8.27443),
-      sc.b = c(2.3033, 2.860176, 2.863716, 2.863716, 2.5802, 2.5802, 2.654215)
+      clam.region = c('South', 'GBK'),
+      oq.a = c(NA_real_, NA_real_),
+      oq.b = c(NA_real_, NA_real_),
+      sc.a = c(NA_real_, NA_real_),
+      sc.b = c(NA_real_, NA_real_)
     )
+    
     coeff[, clam.region := as.factor(clam.region)]
     clamdat <- base::merge(clamdat, coeff, by = 'clam.region')
-
+    
     #Lengths need to be in mm for formula to give g.  Divide by 1000 to get results in kg
     clamdat[SVSPP == 403, meatwt := (exp(sc.a) * (LENGTH * 10)^sc.b) / 1000]
     clamdat[SVSPP == 409, meatwt := (exp(oq.a) * (LENGTH * 10)^oq.b) / 1000]
     clamdat[, expmw := meatwt * NUMLEN]
-    clamdat[,
-      stamw := sum(expmw),
-      by = c('CRUISE6', 'STRATUM', 'STATION', 'SVSPP')
-    ]
+    clamdat[, stamw := sum(expmw, na.rm = TRUE), by = c('CRUISE6', 'STRATUM', 'STATION', 'SVSPP')]
+    
     clamdat[, c('oq.a', 'oq.b', 'sc.a', 'sc.b', 'meatwt', 'expmw') := NULL]
     data.table::setnames(clamdat, "stamw", "BIOMASS.MW")
   }
-  #saveRDS(clamdat, file = here::here('data","Clamdat.RDS'))
+  
   if (tidy) {
     clamdat <- tibble::as_tibble(clamdat)
   }
-
+  
   sql <- list(
     cruise = cruise.qry,
     station = station.qry,
     catch = catch.qry,
     length = length.qry
   )
-
+  
   return(list(
     data = clamdat,
     sql = sql,
